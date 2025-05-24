3e8166fe1ad0a8e74c6b5ef6ff311508e270f4e9
komeil <komeilmohammadian1376.web@gmail.com>
2025-05-24 17:56:20 +0330

    fix:(remove example)

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,3 @@ import lib from './lib';
 
 const kmRouter = lib;
 export default kmRouter;
-
-const routes = kmRouter.makeRoutes([
-  {
-    name: 'home',
-    path: '/home',
-    target: () => {},
-  },
-  {
-    name: 'login',
-    path: '/login',
-    target: () => {},
-  },
-  {
-    name: 'dashboard',
-    path: '/dashboard',
-    target: () => {},
-  },
-]);
-const router = kmRouter.makeRouter(routes, { defaultRoute: 'home' });
-
-router.history.subscribe((history) => {
-  console.log('history:', history);
-});
