16538a1d165bd39c8956c638166e3b87ff860026
komeil <komeilmohammadian1376.web@gmail.com>
2025-05-26 18:27:40 +0330

    fix:(typeof path)

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,3 @@ import lib from './lib';
 
 const kmRouter = lib;
 export default kmRouter;
-
-const routes = kmRouter.makeRoutes([
-  {
-    name: 'generator',
-    path: '/generator',
-    target: () => '',
-  },
-  {
-    name: 'vpn',
-    path: '/vpn',
-    target: () => 2,
-  },
-]);
-
-let x = kmRouter.makeRouter(routes, {});
